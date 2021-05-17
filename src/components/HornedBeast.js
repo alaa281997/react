7d46244704ca54ddcd27921ce766b4b7c706a1dc
alaa281997 <alaabbas97@gmail.com>
2021-05-17 18:01:47 +0300

    lab2

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,31 +1,58 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 
 class HornedBeast extends React.Component {
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         name: 'Sherry',
+    //         imgUrl: sherry
+    //     }
+    // }
+    constructor(props) {
+        super(props);
+        this.state = {
+            numberOfVotes: 0,
+        }
+    }
+
+    numOfVotes = () => {
+        this.setState({
+            numberOfVotes: this.state.numberOfVotes + 1,
+        })
+    }
+
     render() {
         return (
-            <div>
-                <main>
-                    <div>
-                        <div>
-                            <h2>UniWhal</h2>
-                            <img  className='img' src="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"
-                                title="UniWhal" alt="UniWhal" height="500px" width="500px"
-                            />
-                            <p>A unicorn and a narwhal nuzzling their horns</p>
-                        </div>
-                        <div>
-                            <h2>Rhino Family</h2>
-                            <img src="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
-                                title="Rhino Family" alt="Rhino Family" height="500px" width="500px"
-                            />
-                            <p>Mother (or father) rhino with two babies</p>
 
-                        </div>
-                    </div>
-                </main>
+    <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={this.props.image_url} alt={this.props.title}/>
+          <Card.Body>
+            <Card.Title>{this.props.title}</Card.Title>
+            <Card.Text>
+              {this.props.description}
+            </Card.Text>
+          </Card.Body>
+          <Card.Text>Number of votes : {this.state.numberOfVotes} </Card.Text>
+          <Button variant="primary"  onClick={this.numOfVotes}>Votes</Button>
+          
+        </Card>
+        
+            // <div><p>{this.props.title}</p>
+            //     <img src={this.props.image_url} alt={this.props.title} />
+            //     <p>{this.props.description}</p>
+            //     <p> Number of votes :{this.state.numberOfVotes}</p>
+            //     <button onClick={this.numOfVotes}>Votes</button>
+            // </div>
 
-            </div>
+
+
+
+
 
         )
     }
